43f9b21fa6859712f09a99b8508dbc21256ece0c
chardbres <richard.m.bresnahan@gmail.com>
2019-09-27 16:30:43 -0400

    Created initial gameplay logic for switching player turns, capturing spaces, and checking the win/loss/tie status of the game.
    In game/events.js, created functions that:
      1. switchPlayer: identify which player is taking their turn, and switches to the other player after clicking a space.
      2. claimSpace: checks whether no player has claimed the space yet, and if not, claims the space for the current player.
      3. checkGame: checks the status of the game, returns a win condition if a player wins, and a tie condition if nobody wins.

---
+++ assets/scripts/game/events.js
@@ -0,0 +1,104 @@
+'use strict'
+
+const ui = require('./ui.js')
+
+// Initialize empty array to act as the game board
+const gameBoard = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+const playerOne = 'X'
+const playerTwo = 'O'
+// Start the game with X as the current player
+let currentPlayer = playerOne
+let counter = 0
+
+// Function to switch players after clicking on a space
+const switchPlayer = function () {
+  if (currentPlayer === playerOne) {
+    currentPlayer = playerTwo
+    console.log('Player is now: ' + currentPlayer)
+  } else if (currentPlayer === playerTwo) {
+    currentPlayer = playerOne
+    console.log('Player is now: ' + currentPlayer)
+  }
+  return currentPlayer
+}
+
+// Function to claim the space
+const claimSpace = function () {
+  let val
+  switch ($(this).attr('id')) {
+    case 'one':
+      val = 0
+      break
+    case 'two':
+      val = 1
+      break
+    case 'three':
+      val = 2
+      break
+    case 'four':
+      val = 3
+      break
+    case 'five':
+      val = 4
+      break
+    case 'six':
+      val = 5
+      break
+    case 'seven':
+      val = 6
+      break
+    case 'eight':
+      val = 7
+      break
+    case 'nine':
+      val = 8
+  }
+
+  if (gameBoard[val] === 'X' || gameBoard[val] === 'O') {
+    console.log('Illegal move!')
+  } else {
+    gameBoard[val] = currentPlayer
+    $(this).text(currentPlayer)
+    switchPlayer()
+  }
+  checkGame(gameBoard)
+}
+
+// Function to check game win/loss/tie status
+const checkGame = function (gameBoard) {
+  let winCondition = false
+  if (winCondition === false && counter <= 9) {
+    counter += 1
+    if (gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2]) {
+      winCondition = true
+    } else if (gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5]) {
+      winCondition = true
+    } else if (gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8]) {
+      winCondition = true
+    } else if (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6]) {
+      winCondition = true
+    } else if (gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7]) {
+      winCondition = true
+    } else if (gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8]) {
+      winCondition = true
+    } else if (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8]) {
+      winCondition = true
+    } else if (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6]) {
+      winCondition = true
+    } else if (winCondition === false && counter === 9) {
+      console.log("It's a tie!")
+      return winCondition
+    }
+    console.log(counter)
+    if (winCondition === true) {
+      Window.alert('Winner!')
+      return winCondition
+    }
+  }
+}
+
+module.exports = {
+  switchPlayer,
+  claimSpace,
+  checkGame
+}
